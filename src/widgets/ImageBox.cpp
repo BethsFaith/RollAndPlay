f3f32047edbfde5604322e55f71c7b2c83adbece
BethsFaith <BethsFaith7@gmail.com>
2024-02-09 11:41:21 +0300

    Class for non-changing image

---
+++ src/widgets/ImageBox.cpp
@@ -0,0 +1,51 @@
+//
+// Created by BethsFaith on 08.02.2024.
+//
+
+#include "ImageBox.hpp"
+
+namespace Widgets {
+    ImageBox::ImageBox(const GraphicLib::Primitives::AbstractPrimitive::Ptr& graphicPrimitive) : Widget(IMAGE_BOX),
+          _form(graphicPrimitive) {}
+
+    bool ImageBox::checkSelecting(unsigned int x, unsigned int y) {
+        return false;
+    }
+
+    void ImageBox::draw(GraphicLib::Shaders::ShaderProgram::Ptr formShader,
+                        GraphicLib::Shaders::ShaderProgram::Ptr textShader,
+                        GraphicLib::Shaders::ShaderProgram::Ptr pickShader) {}
+
+    void ImageBox::setTransform(glm::vec2 position, glm::vec2 scale) {
+        _form.setTransform(position, scale);
+    }
+
+    void ImageBox::setTransform(glm::vec2 position) {
+        _form.setTransform(position);
+    }
+
+    void ImageBox::setScale(glm::vec2 scale) {
+        _form.setScale(scale);
+    }
+
+    void ImageBox::setImage(const std::string& texturePath, int textureIndex) {
+        _texture = std::make_shared<GraphicLib::Textures::Texture>("", texturePath);
+        GraphicLib::Textures::load2d(*_texture,
+                                     {{.name = GL_TEXTURE_MIN_FILTER, .value = GL_NEAREST},
+                                      {.name = GL_TEXTURE_MAG_FILTER, .value = GL_NEAREST},
+                                      {.name = GL_TEXTURE_WRAP_S, .value = GL_CLAMP_TO_EDGE},
+                                      {.name = GL_TEXTURE_WRAP_T, .value = GL_CLAMP_TO_EDGE},
+                                      {.name = GL_TEXTURE_WRAP_R, .value = GL_CLAMP_TO_EDGE}});
+        _texture->setPath(texturePath);
+
+        _form.setTexture(_texture, textureIndex);
+    }
+
+    glm::vec2 ImageBox::getScale() {
+        return _form.getScale();
+    }
+
+    glm::vec2 ImageBox::getPosition() {
+        return _form.getPosition();
+    }
+}    //namespace Widgets
---
+++ src/widgets/ImageBox.hpp
@@ -0,0 +1,41 @@
+//
+// Created by BethsFaith on 08.02.2024.
+//
+
+#ifndef INC_1_BIN_IMAGEBOX_HPP
+#define INC_1_BIN_IMAGEBOX_HPP
+
+#include <GraphicLib/Textures/Loader.hpp>
+#include <GraphicLib/Textures/Texture.hpp>
+
+#include "Widget.hpp"
+#include "graphic/Form.hpp"
+
+namespace Widgets {
+    class ImageBox : public Widget {
+    public:
+        explicit ImageBox(const GraphicLib::Primitives::AbstractPrimitive::Ptr& graphicPrimitive);
+        ~ImageBox() override = default;
+
+        bool checkSelecting(unsigned int x, unsigned int y) override;
+        void draw(GraphicLib::Shaders::ShaderProgram::Ptr formShader,
+                  GraphicLib::Shaders::ShaderProgram::Ptr textShader,
+                  GraphicLib::Shaders::ShaderProgram::Ptr pickShader) override;
+
+        void setTransform(glm::vec2 position, glm::vec2 scale) override;
+        void setTransform(glm::vec2 position) override;
+        void setScale(glm::vec2 scale) override;
+
+        glm::vec2 getScale() override;
+        glm::vec2 getPosition() override;
+
+        void setImage(const std::string& texturePath, int textureIndex);
+
+    private:
+        Graphic::Form _form;
+        GraphicLib::Textures::Texture::Ptr _texture;
+    };
+
+}    //namespace Widgets
+
+#endif    //INC_1_BIN_IMAGEBOX_HPP
